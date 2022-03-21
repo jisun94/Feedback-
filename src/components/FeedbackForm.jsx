23c1b593900afaf942d12bd053a462cc48f07775
jisun94 <jisun94lee@gmail.com>
2022-03-22 02:12:53 +0900

    🚧 Real-time input validation (FeedbackForm)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,22 +4,41 @@ import Button from './shared/Button';
 
 function Feedbackform() {
   const [text, setText] = useState('');
-  const handleTextChange = (e) => {
-    setText(e.target.value);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(value);
   };
+
   return (
     <Card>
-      <form></form>
-      <h2>How would you rate your service with us?</h2>
-      <div className="input-group">
-        <input
-          onChange={handleTextChange}
-          value={text}
-          type="text"
-          placeholder="Write a review"
-        />
-        <Button type="submit">Send</Button>
-      </div>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            value={text}
+            type="text"
+            placeholder="Write a review"
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
   );
 }
