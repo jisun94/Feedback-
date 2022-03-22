916686424c5627229aa5b065e4e75bd9e582c6ed
jisun94 <jisun94lee@gmail.com>
2022-03-23 00:10:08 +0900

    ✨ Navigate

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Post() {
-  const params = useParams();
+  const status = 200;
+  const navigate = useNavigate();
+  const onClick = () => {
+    console.log('hello');
+    navigate('/about');
+  };
+  if (status === 404) {
+    return <Navigate to="./notfount" />;
+  }
   return (
     <div>
-      <h1>Post{params.id} </h1>
-      <p>Name: {params.name}</p>
+      Post
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
