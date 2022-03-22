df2c4f05245401272d3b912fca61f26ebf8e2b22
jisun94 <jisun94lee@gmail.com>
2022-03-23 02:54:17 +0900

    🚧 Edit feedback with useEffect

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -7,20 +7,45 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This iem is from context',
+      text: 'Feedback item1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'Feedback item2',
+      rating: 10,
+    },
+    {
+      id: 3,
+      text: 'Feedback item3',
       rating: 10,
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  //Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
@@ -29,6 +54,8 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        feedbackEdit,
       }}
     >
       {children}
