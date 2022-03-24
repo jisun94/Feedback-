47b4f64a61ce0eac6c6b996bc8fbd38357d24f39
jisun94 <jisun94lee@gmail.com>
2022-03-25 01:37:24 +0900

    🚧 Fetch data from JSON-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'Feedback item1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'Feedback item2',
-      rating: 10,
-    },
-    {
-      id: 3,
-      text: 'Feedback item3',
-      rating: 10,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //Fetch Feedback
+
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback`);
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   //Delete feedback
   const deleteFeedback = (id) => {
@@ -60,6 +57,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
