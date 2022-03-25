cacd165f80d645380ba87b2d45a38b22d0b8ed42
jisun94 <jisun94lee@gmail.com>
2022-03-25 12:54:30 +0900

    ✨ Add, Update, Delete feedback from JSON-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
@@ -18,30 +17,53 @@ export const FeedbackProvider = ({ children }) => {
   //Fetch Feedback
 
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback`);
+    const response = await fetch(`/feedback`);
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
   };
 
   //Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   //Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   //Update feedback item
 
-  const updateFeedback = (id, updItem) => {
-    updItem.id = id;
-    setFeedback(feedback.map((item) => (item.id === id ? updItem : item)));
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
   };
 
   //Set item to be updated
